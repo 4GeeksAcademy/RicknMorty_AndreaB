b62bd6c4f0e5bbd5477453d9f63e82f48719f16a
Drea Q <marcellab721@icloud.com>
2023-11-03 04:13:50 +0000

    created uml for rick and morty

--- src/models.py
+++ src/models.py
@@ -1,32 +1,68 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
+from sqlalchemy.orm import relationship, declarative_base, relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+character_episode_table = Table(
+    "character_episode_table",
+    Base.metadata,
+    Column("character_id", ForeignKey("character.id")),
+    Column("episode_id", ForeignKey("episode.id")),
+    )
+
+character_location_table = Table(
+    "character_location_table",
+    Base.metadata,
+    Column("character_id", ForeignKey("character.id")),
+    Column("location_id", ForeignKey("location.id")),
+    )
+
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    image = Column(String(250), nullable=False)
+    species = Column(String (100), nullable=False)
+    status = Column(String(50), nullable=False)
+    episode = relationship("Episode", secondary=character_episode_table, back_populates="charaters")
+    type = Column(String(50), nullable=False)
+    url = Column(String(250), nullable=False)
+    location = relationship("Location")
+    origin = relationship("Location")
 
-class Address(Base):
-    __tablename__ = 'address'
+
+
+class Episode(Base):
+    __tablename__ = 'episode'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    episode = Column(String(50), nullable=False)
+    air_date = Column(String(20), nullable=False)
+    created = Column(String(50), nullable=False)
+    episode = Column(String(50), nullable=False)
+    name  =  Column(String(50), nullable=False)
+    url = Column(String(250), nullable=False)
+    characters = relationship("Character", secondary=character_episode_table, back_populates="episodes")
 
-    def to_dict(self):
-        return {}
+
+class Location(Base):
+     __tablename__ = 'location'
+     id = Column(Integer, primary_key=True)
+     created = Column(String(40), nullable=False)
+     dimension = Column(String(50), nullable=False)
+     name = Column(String(50), nullable=False)
+     residents = relationship("Character")
+     type = Column(String(50), nullable=False)
+     url = Column(String(250), nullable=False)
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
